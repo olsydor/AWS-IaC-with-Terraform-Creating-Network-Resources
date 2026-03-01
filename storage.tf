71cd7657f3a4d6452c81e8bc30b6bbceffc21611
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-03-02 00:20:30 +0200

    remove old files

--- storage.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "cmtr-5bc36296-bucket" {
-  count  = var.enable_legacy_resources ? 1 : 0
-  bucket = "cmtr-5bc36296-bucket-1772098289"
-
-  tags = {
-    Project = "cmtr-5bc36296"
-  }
-}
