fc3f834b9592bb8bf2fc3f34bbc0b3311d5a1975
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-03-02 11:51:21 +0200

    Standardize Terraform version checks in versions.tf

--- tf_code/main.tf
+++ tf_code/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = ">= 1.5.7"
-
-  backend "s3" {}
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
