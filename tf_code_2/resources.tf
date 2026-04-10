d8c8e506930ebc489e60fbb52bc7d8f5a4d57df0
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-04-10 12:12:20 +0300

    Fix resources.tf to match actual AWS resource, restore data.tf with guards

--- tf_code_2/resources.tf
+++ tf_code_2/resources.tf
@@ -1,15 +1,13 @@
 resource "aws_iam_policy" "custom_policy" {
-  name = "custom_policy"
+  name        = "resource-move-demo-policy"
+  description = "Custom role with limited permissions"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
+        Effect   = "Allow"
+        Action   = ["ec2:*", "s3:*"]
         Resource = "*"
       }
     ]
