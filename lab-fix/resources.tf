0487c7791582bc24970d9f3d62a13fbff9b30112
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-04-14 14:33:51 +0000

    Migrate state backend to new S3 bucket

---
+++ lab-fix/resources.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_policy" "custom_policy" {
+  name = "custom_policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{ Effect = "Allow", Action = ["s3:GetObject", "s3:ListBucket"], Resource = "*" }]
+  })
+  lifecycle { ignore_changes = [all] }
+}
