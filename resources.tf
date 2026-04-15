0ceb30bd10a3fe07cc293595e2fefa34a9819783
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-04-15 19:51:17 +0300

    align resources.tf with existing policy

--- resources.tf
+++ resources.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_policy" "custom_policy" {
-  name = "custom_policy"
+  name        = "resource-move-demo-policy"
+  description = "Custom role with limited permissions"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -7,8 +8,8 @@ resource "aws_iam_policy" "custom_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
+          "ec2:*",
+          "s3:*"
         ]
         Resource = "*"
       }
