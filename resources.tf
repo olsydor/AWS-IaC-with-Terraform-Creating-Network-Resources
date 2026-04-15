88e9857569ea8b163013ea80b5eca63dd3f27609
Oleksandr Sydor <oleksandr_sydor@epam.com>
2026-04-15 19:09:44 +0300

    fix init prompt and restore policy config

--- resources.tf
+++ resources.tf
@@ -1,15 +1,21 @@
 resource "aws_iam_policy" "custom_policy" {
-  name        = "resource-move-demo-policy"
-  description = "Custom role with limited permissions"
+  name = "custom_policy"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = ["ec2:*", "s3:*"]
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
         Resource = "*"
       }
     ]
   })
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
